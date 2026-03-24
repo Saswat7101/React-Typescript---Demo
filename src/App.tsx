57f145b1663a6e51e8b6ef593152aaf1fb4099c8
Saswat <mohantysaswat154@gmail.com>
2026-03-24 09:09:22 +0530

    removing a todo

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,21 @@ import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([
-    new Todo("Learn React"),
-    new Todo("Learn Typescript"),
-  ]);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (todoText: string) => {
     const newTodo = new Todo(todoText);
     setTodos((prevTodos) => prevTodos.concat(newTodo));
   };
 
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
+  };
+
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
